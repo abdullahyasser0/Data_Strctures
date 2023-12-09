1dc05453a502d51cc951fde12cd53d2ba99027b4
Abdullah <s-abdullah.yasser@zewailcity.edu.eg>
2023-12-09 15:50:12 +0200

    Binarysearch

---
+++ Algorithms/Binarysearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+template<typename T>
+int BinarySearch(T Arr[], int L , int R, T value){
+    int len = L-R+1;
+    int mid = (L+R)/2;
+    if (Arr[mid] == value) return mid;
+    if(len<=1){
+         if (value<Arr[mid]){
+            return BinarySearch(Arr , L, mid-1,value);
+        }
+
+            else if (value>Arr[mid]){
+            return BinarySearch(Arr , mid+1, R,value);
+        }
+    }
+
+return -1;
+}
+
+
+
+
+
+
+
+
+
+int main(){
+
+int arr[5]={1,2,3,4,5};
+
+cout<<BinarySearch (arr,0,4,8);
+    return 0;
+}
